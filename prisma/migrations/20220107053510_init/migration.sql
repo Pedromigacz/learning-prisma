38115c7392e97347c10ccda31edbdc4c2fc1ded9
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-07 02:38:50 -0300

    add prisma scheme

---
+++ prisma/migrations/20220107053510_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Field" (
+    "id" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "sort_index" INTEGER NOT NULL,
+    "field_id" TEXT NOT NULL,
+
+    CONSTRAINT "Field_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Field_label_key" ON "Field"("label");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_name_key" ON "Form"("name");
+
+-- AddForeignKey
+ALTER TABLE "Field" ADD CONSTRAINT "Field_field_id_fkey" FOREIGN KEY ("field_id") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
