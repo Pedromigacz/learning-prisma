aaa25444f20ae5ad836b6197976d9588233fc999
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-07 20:00:44 -0300

    add context and schema

---
+++ src/context.ts
@@ -0,0 +1,24 @@
+import { PrismaClient, User } from "@prisma/client";
+import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
+import { Request, Response } from "express";
+import { prisma } from "./clients";
+import { getUserId } from "./utils/auth";
+
+export interface Context {
+  request: Request;
+  response: Response;
+  prisma: PrismaClient;
+  user: User | null;
+}
+
+export async function createContext(
+  request: ExpressContext
+): Promise<Partial<Context>> {
+  const context: Context = {
+    ...request,
+    response: request.res,
+    request: request.req,
+  };
+
+  return context;
+}
