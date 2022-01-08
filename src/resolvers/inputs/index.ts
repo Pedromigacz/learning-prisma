d27335ef88546d7ec7e18fb2736824cea75f7244
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-08 02:41:07 -0300

    add input validations

--- src/resolvers/inputs/index.ts
+++ src/resolvers/inputs/index.ts
@@ -1,8 +1,32 @@
 import { inputObjectType } from "nexus";
 
+// Form
 export const FormWhereUniqueInput = inputObjectType({
   name: "FormWhereUniqueInput",
   definition(t) {
     t.nonNull.id("id");
+  },
+});
+
+export const CreateFormInput = inputObjectType({
+  name: "CreateFormInput",
+  definition(t) {
+    t.nonNull.string("name");
+  },
+});
+
+// FormField
+export const FormFieldWhereUniqueInput = inputObjectType({
+  name: "FormFieldWhereUniqueInput",
+  definition(t) {
+    t.nonNull.id("id");
+  },
+});
+
+export const CreateFormFieldInput = inputObjectType({
+  name: "CreateFormFieldInput",
+  definition(t) {
+    t.nonNull.string("label");
+    t.nonNull.int("sort_index");
   },
 });
